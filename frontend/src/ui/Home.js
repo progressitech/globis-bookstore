5bb26a8fd1075644b99d254abb2044dc42561220
Stephen Wade <swade6@cnm.edu>
2022-08-05 13:15:37 -0600

    added spacing and padding in Coming Up/Book Launch container

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -6,7 +6,7 @@ export function Home() {
   return (
     <>
       <Container
-        className="container-1-image text-dark bg-opacity-10 rounded my-5 p-4"
+        className="container-1-image text-dark bg-opacity-10 rounded my-3 p-4"
       >
         <h1 className="container-1-h1">Special deals, <br /> Interesting <br />Books, <br />At best prices.</h1>
       </Container>
@@ -15,7 +15,7 @@ export function Home() {
                     <Avengers/>
                 </Row>
             </Container>
-            <Container className="px-5">
+            <Container className="px-5 pb-5">
                 <Row>
                     <Col md={12} className="text-center pb-5">
                         <h5>Coming Up</h5>
@@ -27,6 +27,7 @@ export function Home() {
                         <h5>Introducing "Plausible Accolade"</h5>
                         <h6>By Samuel Kumi Asiedu</h6>
                         <h6>When</h6>
+                        <hr />
                     </Col>
                     <Col md={6}>
                         <Image fluid src="https://innovating.capital/wp-content/uploads/2021/05/vertical-placeholder-image.jpg" className="place-holder-image-1" alt="Place-holder Book Cover" />
