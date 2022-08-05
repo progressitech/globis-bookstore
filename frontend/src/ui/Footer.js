917d5ce07c82c70ffe7b6cdd8eb3ada4abec3714
Stephen Wade <swade6@cnm.edu>
2022-08-05 14:41:37 -0600

    started working on the Footer

--- frontend/src/ui/Footer.js
+++ frontend/src/ui/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Col, Row } from "react-bootstrap";
 
 export function Footer(){
     return (
-        <footer className='bg-dark text-center py-4'>
-            <p className="text-light mb-0">This is my footer</p>
+        <footer className='bg-light text-center py-4'>
+            <Row>
+                <Col md={3}>
+                <p>Test Column 1</p>
+                </Col>
+                <Col md={3}>
+                <p>Test Column 2</p>
+                </Col>
+                <Col md={3}>
+                <p>Test Column 3</p>
+                </Col>
+                <Col md={3}>
+                <p>Test Column 4</p>
+                </Col>
+            </Row>
         </footer>
     )
 }
