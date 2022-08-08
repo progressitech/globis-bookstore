183349f867bc6476efbf2daaabb8b8bde3b962af
Stephen Wade <swade6@cnm.edu>
2022-08-08 10:17:16 -0600

    fixed footer

--- frontend/src/ui/Footer.js
+++ frontend/src/ui/Footer.js
@@ -1,47 +1,96 @@
-import React from 'react'
-import { Col, Container, Row } from "react-bootstrap";
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "./App.css";
 
-export function Footer(){
-    return (
-        <footer className='bg-dark text-center text-light py-4'>
-            <Container>
-            <Row>
-                <Col md={3}>
-                <p>Globis Bookstore</p>
-                </Col>
-                <Col md={3}>
-                <p>Shop</p>
-                </Col>
-                <Col md={3}>
-                <p>Socials</p>
-                </Col>
-                <Col md={3}>
-                <p>Be The First To Know</p>
-                </Col>
-            </Row>
-            <Row>
+export function Footer() {
+  return (
+    <Container fluid className="footer-background-color">
+      <Container className="bg-opacity-100 py-4">
+        <footer className="text-light text-center py-4">
+          <Row>
             <Col md={3}>
-                <p>Bealeton, Virginia, USA <br /> +1 (571) 376-3319 <br /> samuelasiedu@hotmail.com</p>
-                </Col>
-                <Col md={3}>
-                <p>FAQ</p>
-                <p>Shipping & Returns</p>
-                <p> Store Policy</p>
-                <p>Payment Methods</p>
-                </Col>
-                <Col md={3}>
-                <p>Facebook</p>
-                <p>Twitter</p>
-                <p>Instagram</p>
-                <p>Pinterest</p>
-                </Col>
-                <Col md={3}>
-                <p>Sign up for our newsletter</p>
-                <button className="bg-transparent border-light px-4 py-2 text-light">Enter your email here*</button>
-                <button className="bg-light px-4 py-2 text-dark">Subscribe</button>
-                </Col>
-            </Row>
-            </Container>
+              <p>Globis Bookstore</p>
+              <br />
+              <p>Bealeton, Virginia, USA</p>
+              <p>+1 (571) 376-3319</p>
+              <a
+                href="mailto:samuelasiedu@hotmail.com"
+                className="link-unstyled"
+              >
+                samuelasiedu@hotmail.com
+              </a>
+            </Col>
+            <Col md={3}>
+              <p>Shop</p>
+              <br />
+              <p>
+                <a href="/globisbookstore/faq" className="link-unstyled">
+                  FAQ
+                </a>
+              </p>
+              <p>
+                <a
+                  href="/globisbookstore/shipping-returns"
+                  className="link-unstyled"
+                >
+                  Shipping & Returns
+                </a>
+              </p>
+              <p>
+                <a
+                  href="/globisbookstore/store-policy"
+                  className="link-unstyled"
+                >
+                  Store Policy
+                </a>
+              </p>
+            </Col>
+            <Col md={3}>
+              <p>Socials</p>
+              <br />
+              <p>
+                <a href="https://www.facebook.com/" className="link-unstyled">
+                  Facebook
+                </a>
+              </p>
+              <p>
+                <a href="https://www.twitter.com" className="link-unstyled">
+                  Twitter
+                </a>
+              </p>
+              <p>
+                <a href="https://www.instagram.com" className="link-unstyled">
+                  Instagram
+                </a>
+              </p>
+              <p>
+                <a href="https://www.pinterest.com" className="link-unstyled">
+                  Pinterest
+                </a>
+              </p>
+            </Col>
+            <Col md={3}>
+              <p>Be The First To Know</p>
+              <br />
+              <p>Sign up for our newsletter</p>
+              <form method="POST">
+                <p>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your email here"
+                    className="footer-background-color text-light ps-2"
+                  />
+                </p>
+                <p>
+                  <button type="submit" className="footer-submit-text-color border border-none p-1">Subscribe</button>
+                </p>
+              </form>
+            </Col>
+          </Row>
         </footer>
-    )
+      </Container>
+    </Container>
+  );
 }
