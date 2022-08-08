c4769f65b7ce222838e5f8364679d3cebf117a12
DeonB <DeonButler44@gmail.com>
2022-08-08 11:55:40 -0400

    Footer built, css profile created for reoccuring colors.

--- frontend/src/ui/Footer.js
+++ frontend/src/ui/Footer.js
@@ -1,23 +1,96 @@
-import React from 'react'
-import { Col, Row } from "react-bootstrap";
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "./App.css";
 
-export function Footer(){
-    return (
-        <footer className='bg-light text-center py-4'>
-            <Row>
-                <Col md={3}>
-                <p>Test Column 1</p>
-                </Col>
-                <Col md={3}>
-                <p>Test Column 2</p>
-                </Col>
-                <Col md={3}>
-                <p>Test Column 3</p>
-                </Col>
-                <Col md={3}>
-                <p>Test Column 4</p>
-                </Col>
-            </Row>
+export function Footer() {
+  return (
+    <Container fluid className="footer-background-color">
+      <Container className="bg-opacity-100 py-4">
+        <footer className="text-light text-center py-4">
+          <Row>
+            <Col md={3}>
+              <p>Globis Bookstore</p>
+              <br />
+              <p>Bealeton, Virginia, USA</p>
+              <p>+1 (571) 376-3319</p>
+              <a
+                href="mailto:samuelasiedu@hotmail.com"
+                className="link-unstyled"
+              >
+                samuelasiedu@hotmail.com
+              </a>
+            </Col>
+            <Col md={3}>
+              <p>Shop</p>
+              <br />
+              <p>
+                <a href="/globisbookstore/faq" className="link-unstyled">
+                  FAQ
+                </a>
+              </p>
+              <p>
+                <a
+                  href="/globisbookstore/shipping-returns"
+                  className="link-unstyled"
+                >
+                  Shipping & Returns
+                </a>
+              </p>
+              <p>
+                <a
+                  href="/globisbookstore/store-policy"
+                  className="link-unstyled"
+                >
+                  Store Policy
+                </a>
+              </p>
+            </Col>
+            <Col md={3}>
+              <p>Socials</p>
+              <br />
+              <p>
+                <a href="https://www.facebook.com/" className="link-unstyled">
+                  Facebook
+                </a>
+              </p>
+              <p>
+                <a href="https://www.twitter.com" className="link-unstyled">
+                  Twitter
+                </a>
+              </p>
+              <p>
+                <a href="https://www.instagram.com" className="link-unstyled">
+                  Instagram
+                </a>
+              </p>
+              <p>
+                <a href="https://www.pinterest.com" className="link-unstyled">
+                  Pinterest
+                </a>
+              </p>
+            </Col>
+            <Col md={3}>
+              <p>Be The First To Know</p>
+              <br />
+              <p>Sign up for our newsletter</p>
+              <form method="POST">
+                <p>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your email here"
+                    className="footer-background-color text-light ps-2"
+                  />
+                </p>
+                <p>
+                  <button type="submit" className="footer-submit-text-color border border-none p-1">Subscribe</button>
+                </p>
+              </form>
+            </Col>
+          </Row>
         </footer>
-    )
+      </Container>
+    </Container>
+  );
 }
--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -5,42 +5,49 @@ import { Avengers } from "./Avengers";
 export function Home() {
   return (
     <>
-      <Container
-        className="container-1-image text-dark bg-opacity-10 rounded my-3 p-4"
-      >
-        <h1 className="container-1-h1">Special deals, <br /> Interesting <br />Books, <br />At best prices.</h1>
+      <Container className="container-1-image text-dark bg-opacity-10 rounded my-3 p-4">
+        <h1 className="container-1-h1">
+          Special deals, <br /> Interesting <br />
+          Books, <br />
+          At best prices.
+        </h1>
       </Container>
       <Container>
-                <Row>
-                    <Avengers/>
-                </Row>
-            </Container>
-            <Container className="px-5 pb-5">
-                <Row>
-                    <Col md={12} className="text-center pb-5">
-                        <h5>Coming Up</h5>
-                        <h1>BOOK LAUNCH</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6} className="book-launch-text-column">
-                        <h5 className="mb-4">Introducing "Plausible Accolade"</h5>
-                        <h6 className="mb-4">By Samuel Kumi Asiedu</h6>
-                        <br />
-                        <h6 className="mb-2">When</h6>
-                        <hr />
-                        <h6 className="mb-4">Jul 12, 2023, 7:00 AM</h6>
-                        <br />
-                        <h6 className="mb-2">Where</h6>
-                        <hr />
-                        <h6 className="mb-5">Video Conference</h6>
-                        <button className="bg-transparent px-4 py-2">RSVP NOW</button>
-                    </Col>
-                    <Col md={6}>
-                        <Image fluid src="https://innovating.capital/wp-content/uploads/2021/05/vertical-placeholder-image.jpg" className="place-holder-image-1" alt="Place-holder Book Cover" />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+        <Row>
+          <Avengers />
+        </Row>
+      </Container>
+      <Container className="px-5 pb-5">
+        <Row>
+          <Col md={12} className="text-center pb-5">
+            <h5>Coming Up</h5>
+            <h1>BOOK LAUNCH</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} className="book-launch-text-column">
+            <h5 className="mb-4">Introducing "Plausible Accolade"</h5>
+            <h6 className="mb-4">By Samuel Kumi Asiedu</h6>
+            <br />
+            <h6 className="mb-2">When</h6>
+            <hr />
+            <h6 className="mb-4">Jul 12, 2023, 7:00 AM</h6>
+            <br />
+            <h6 className="mb-2">Where</h6>
+            <hr />
+            <h6 className="mb-5">Video Conference</h6>
+            <button className="bg-transparent px-4 py-2">RSVP NOW</button>
+          </Col>
+          <Col md={6}>
+            <Image
+              fluid
+              src="https://innovating.capital/wp-content/uploads/2021/05/vertical-placeholder-image.jpg"
+              className="place-holder-image-1"
+              alt="Place-holder Book Cover"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
