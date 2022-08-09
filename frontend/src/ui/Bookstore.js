e179fe66932f37f1a37ca1c1286656b6291ca00e
Stephen Wade <swade6@cnm.edu>
2022-08-09 09:13:25 -0600

    see above

--- frontend/src/ui/Bookstore.js
+++ frontend/src/ui/Bookstore.js
@@ -13,17 +13,27 @@ export function Bookstore() {
       </Container>
       <Container fluid>
         <Row className="d-flex px-5">
-          <Col md={3}></Col>
+          <Col md={3}>
+            <h4>Filter by</h4>
+            <hr />
+          </Col>
           <Col md={9} className="d-flex justify-content-end">
-            <DropdownButton 
-            id="dropdown-basic-button" 
-            title="Sort by"
-            variant="outline-dark border-dark rounded-0"
-            className="bookstore-dropdown-button">
-              <Dropdown.ItemText className="bg-secondary">Sort by</Dropdown.ItemText>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Sort by"
+              variant="outline-dark border-dark rounded-0"
+              className="bookstore-dropdown-button"
+            >
+              <Dropdown.ItemText className="bg-secondary">
+                Sort by
+              </Dropdown.ItemText>
               <Dropdown.Item href="#/action-1">Newest</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Price (low to high)</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Price (high to low)</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">
+                Price (low to high)
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3">
+                Price (high to low)
+              </Dropdown.Item>
               <Dropdown.Item href="#/action-3">Name A-Z</Dropdown.Item>
               <Dropdown.Item href="#/action-3">Name Z-A</Dropdown.Item>
             </DropdownButton>
